b2fd9d03fb5dfe7c6bfb84230ff3d50ee1527254
souzagabi <souza_gabi@yahoo.com.br>
2023-01-27 10:39:25 -0300

    Versão 8.1 Dois Banners Automatico

--- index.php
+++ index.php
@@ -4,98 +4,70 @@
 
   <head>
 
-      <!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-9XHPWWBF0L"></script>
-	<script>
-  		window.dataLayer = window.dataLayer || [];
-  		function gtag(){dataLayer.push(arguments);}
-  		gtag('js', new Date());
-
-  		gtag('config', 'G-9XHPWWBF0L');
-	</script>
-
-	<!-- Event snippet for Website traffic conversion page -->
-	<script>
-  		gtag('event', 'conversion', {'send_to': 'AW-972803889/XJMJCMrpioAYELGe788D'});
-	</script>
+    <!-- Google tag (gtag.js) -->
+	<?php	require_once("./php/googletag.php");	?>
 
     <?php require_once('partsIndex/header.html'); ?>
+	<script type="text/javascript" src="./js/jquery-1.8.0.min.js"></script>
+	<script type="text/javascript" src="./banner.js"></script>
+
   </head>
   
   <body>
 
-      <!-- Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
 
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGBJWHR"
-
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
-<!-- End Google Tag Manager (noscript) -->
+	<?php require_once("./js/noscriptgoogle.js");	?>
+	<!-- End Google Tag Manager (noscript) -->
 
 	<!-- Incício da sessão do banner-->
     <section class="section-top-banner">
-      <div class="top-banner">
-		<?php require_once('partsIndex/top.html'); ?>
-		<div class="banner">
+		<div class="top-banner">
+			<?php require_once('partsIndex/top.html'); ?>
 			<!-- Banner Section -->
-			<section id="banner">
-				<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-					<ol class="carousel-indicators">
-						<li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
-						<li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
-						<li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
-					</ol>
-					<div class="carousel-inner">
-						<div class="carousel-item active">
-							<img src="./img/BannerRainhaBolao.png" alt="FIXO" class="d-block w-100" alt="..." />
-							<div class="carousel-caption d-none d-md-block">
-								<p>Horário de atendimento de segunda a sábado das 9:00 às 17:00.</p>
-							</div>
-						</div>
-						<div class="carousel-item">
-							<img src="https://www.markuptag.com/images/slider-img-2.jpg" class="d-block w-100" alt="..." />
-							<div class="carousel-caption d-none d-md-block">
-								<p>Horário de atendimento de segunda a sábado das 9:00 às 17:00.</p>
-							</div>
-						</div>
-						<div class="carousel-item">
-							<img src="https://www.markuptag.com/images/slider-img-3.jpg" class="d-block w-100" alt="..." />
-							<div class="carousel-caption d-none d-md-block">
-								<p>Horário de atendimento de segunda a sábado das 9:00 às 17:00.</p>
-							</div>
+			<div class="banner">
+				<section class="container"  id="banner_geral">
+					<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+						<ol id="carousel-indicators" class="carousel-indicators"></ol>
+						<div id="carousel-inner" class="carousel-inner"></div>
+						<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+							<img src="./img/imgFixa/SETA_ESQUERDA.png" alt="Seta para mudar imagem para esquerda" srcset="">
+							<span class="visually-hidden">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+							<img src="./img/imgFixa/SETA_DIREITA.png" alt="Seta para mudar imagem para direita" srcset="">
+							<span class="visually-hidden">Next</span>
+						</a>
+						<div class="carousel-caption d-none d-md-block">
+							<p>Horário de atendimento de segunda a sábado das 10:00 às 18:00.</p>
 						</div>
 					</div>
-					<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
-						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-						<span class="visually-hidden">Previous</span>
-					</a>
-					<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
-						<span class="carousel-control-next-icon" aria-hidden="true"></span>
-						<span class="visually-hidden">Next</span>
-					</a>
-				</div>
-			</section>
+				</section>
+			</div>
+			<div class="banner-mobile">
+				<section class="container"  id="banner_mobile">
+					<div id="carouselExampleCaptions-mobile" class="carousel slide" data-bs-ride="carousel">
+						<ol id="carousel-indicators-mobile" class="carousel-indicators"></ol>
+						<div id="carousel-inner-mobile" class="carousel-inner"></div>
+						<a class="carousel-control-prev" href="#carouselExampleCaptions-mobile" role="button" data-bs-slide="prev">
+							<img src="./img/imgFixa/SETA_ESQUERDA.png" alt="Seta para mudar imagem para esquerda" srcset="">
+							<span class="visually-hidden">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#carouselExampleCaptions-mobile" role="button" data-bs-slide="next">
+							<img src="./img/imgFixa/SETA_DIREITA.png" alt="Seta para mudar imagem para direita" srcset="">
+							<span class="visually-hidden">Next</span>
+						</a>
+					</div>
+				</section>
+			</div>
 			<!-- End Banner Section -->
-			<section id="banner">
-				<div class="swiper-container swiper-container-initialized swiper-container-horizontal">
-					<div class="swiper-button-prev"></div>
-					<div class="swiper-button-next"></div>
-					<div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets">
-						<span class="swiper-pagination-bullet"></span><span
-							class="swiper-pagination-bullet swiper-pagination-bullet-active"></span>
-					</div>
-					<div class="swiper-wrapper" style="transform: translate3d(-2698px, 0px, 0px); transition-duration: 0ms;">
-						<div class="swiper-slide swiper-slide-active" data-swiper-slide-index="1" style="width: 1349px">
-							<figure>
-								<img src="./img/BannerRainhaBolao.png" alt="FIXO" style="width: 1349px" height="200" />
-							</figure>
-						</div>
-					</div>
-				</div>
-			</section>
 		</div>
-      </div>
     </section>
+	<section id="msgHorario">
+		<div class="carousel-caption d-md-block">
+			<p>Horário de atendimento de segunda a sábado das 10:00 às 18:00.</p>
+		</div>
+	</section>
 	<!-- Fim da sessão do banner-->
 	<!-- Início da sessão de conteúdo-->
 	<?php
@@ -134,7 +106,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 							<p>Estamos no mercado desde junho de 1989, há 33 anos no mercado lotérico. </p>
 							<p>Escolhemos estar aqui com você para trazer muita sorte e milhões em prêmios através da comodidade dos serviços pelo WhatsApp 
 								Venha conosco. É fácil, prático e seriedade acima de tudo. Alie sua sorte com a nossa e venha ganhar você também.  Vemmm....</p>
-							<p>Horário de atendimento de segunda a sábado das 9:00 às 17:00.</p>
+							<p>Horário de atendimento de segunda a sábado das 10:00 às 18:00.</p>
 						</div>
 					</article>
 				</div>
@@ -154,7 +126,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 				</div>
 			</div>
 		</section>
-		<!--div class="leading-page" style="background-color: #930089;"><center><img src="./img/leadingpage.jpeg" alt="" srcset=""></center></div-->
 		<div class="game">
 			<div class="cards">
 				<section class="container">
@@ -162,10 +133,10 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 						<?php 
 							/*
 								Fonte Utilizada - Futura LT Bold
-								#2E3078 - Milionárioa
+								#2E3078 - Milionárioa => 01046b
 								#209869 - Mega Sena
 								#930089 - Lotofácil
-								#260085 - Quina
+								#260085 - Quina => 4b025a
 								#f78100 - Lotomania
 								#00ff48 - Timemania
 								#a61324 - Dupla Sena
@@ -194,6 +165,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 									$valorCota		= number_format($ev['value'][$i]['bolao']['valorCota'],2,",",".");
 									$cotaDisponivel = $ev['value'][$i]['qtdeCotasLiberadas'];
 									$nomeDescricao	= $ev['value'][$i]['nomeDescricao'];
+									$promocao		= $ev['value'][$i]['concursoEspecial'];
 									
 									if ($cotaDisponivel > 0) {
 
@@ -201,10 +173,10 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		
 										$texto			= 'Loteria: '.$loteria.'%0ABolão: '.$nomeDescricao.'%0ANº Concurso: '.$numeroConsurso.'%0AValor do Concurso: '.$valorCota.'%0ACotas Disponíveis: '.$cotaDisponivel;
 										
-										if ($loteria == '+MILIONÁRIA') 	{ $corFundo = '#2E3078';} 
+										if ($loteria == '+MILIONÁRIA') 	{ $corFundo = '#01046b';} 
 										if (($loteria == 'MEGA SENA') ||($loteria == 'MEGA-SENA') )	{ $corFundo = '#209869';} 
 										if ($loteria == 'LOTOFÁCIL') 	{ $corFundo = '#930089';} 
-										if ($loteria == 'QUINA') 		{ $corFundo = '#260085';} 
+										if ($loteria == 'QUINA') 		{ $corFundo = '#4b025a';} 
 										if ($loteria == 'LOTOMANIA') 	{ $corFundo = '#f78100';} 
 										if ($loteria == 'TIMEMANIA') 	{ $corFundo = '#00ff48';} 
 										if (($loteria == 'DUPLA SENA') || ($loteria == 'DUPLA-SENA')) { $corFundo = '#a61324';} 
@@ -230,8 +202,14 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 											echo '<div class="row loteria" style="background-color: '.$corFundo.'">';
 											echo '<div class="imgLoteria"><img alt="" class="imgLot" src="./img/'.$nomeImgLoteria.'.jpg"></div>';
 										}
-								
+										if ($promocao == 'TRUE'){
+											$bolaoPromocao  = 'data:image/jpg;base64,'.$ev['value'][$i]['bolao']['imagem'];
+											converter_base64_para_imagem($bolaoPromocao, 'img/', $nomeImagem);
+
+											echo '<div class="col-sm-6 promocao"><img width="100%" style="margin-top: 35%;" src="./img/promocao/'.$nomeImagem.'.jpg"></div>';
+										}
 						?>
+						
 						<div class="column-geral col-sm-3" style="background-color: <?php echo $corFundo; ?>;">
 							<div class="row title">
 								<!-- ====================== Início do Título ====================== -->
@@ -260,7 +238,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 							<!-- ===================== Início do Rodapé ====================== -->
 							<h4><?php echo  $nomeDescricao;?></h4>
 							<div class="botao">
-								<!--a class="btn" data-bs-toggle="modal" onclick="carregar(id)" id="<?php //echo $texto; ?>" data-bs-target="#modalBolao"-->
 								<a class="btn" href="https://api.whatsapp.com/send?phone=<?php echo $telefoneAtendimento; ?>&amp;text=<?php echo $texto?>">
 									<span >
 										<span >
@@ -325,7 +302,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 							<p>Estamos no mercado desde junho de 1989, há 33 anos no mercado lotérico. </p>
 							<p>Escolhemos estar aqui com você para trazer muita sorte e milhões em prêmios através da comodidade dos serviços pelo WhatsApp 
 								Venha conosco. É fácil, prático e seriedade acima de tudo. Alie sua sorte com a nossa e venha ganhar você também.  Vemmm....</p>
-							<p>Horário de atendimento de segunda a sábado das 9:00 às 17:00.</p>
+							<p>Horário de atendimento de segunda a sábado das 10:00 às 18:00.</p>
 						</div>
 					</article>
 				</div>
@@ -349,55 +326,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<!-- Fim da sessão de conteúdo-->
 
 	<?php require_once('partsIndex/footer.html'); ?>
-	<!-- Início do Modal -->
-	<!--div class="modal fade" id="modalBolao" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
-		<div class="modal-dialog modal-dialog-centered" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h3>Iniciar Compra</h3>
-					<button type="button" class="btn btn-danger" data-bs-dismiss="modal" aria-label="Fechar">
-					<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<form action="./php/dados.php" method="POST" target="_blank">
-					<div class="modal-body">
-						<?php 
-
-							/*echo '<div id="modal_bolao"></div>';	
-							echo '<div id="modal_valorCota"></div>';	
-							echo '<div id="modal_cotaDisponivel"></div><hr>';	
-							
-							echo 'Efetuar a transferência do valor da cota para: <br>';	
-							echo 'Empresa: <strong>'.$razaoSocial.'</strong><br>'; 
-							echo 'Chave PIX CNPJ: <strong>'.$chavePix.'</strong>'; */
-						?>
-						<input type="text" name="empresa" id="empresa" hidden value="<?php //echo $razaoSocial; ?>" readonly>
-						<input type="text" name="chavepix" id="chavepix" hidden value="<?php //echo $chavePix; ?>" readonly>
-						
-						<input type="text" name="phone" id="phone" hidden value="<?php //echo $telefoneAtendimento; ?>" readonly>
-						<input type="text" name="modal_text" id="modal_text" hidden readonly>
-
-						<input class="input-modal form-control" type="text"  id="nome" name="nome" placeholder="Nome completo" required>
-						<input class="input-modal form-control" type="text"  id="cpf" name="cpf" placeholder="CPF" required>
-						<input class="input-modal form-control" type="text"  id="telefone" name="telefone" placeholder="Telefone" required>
-						<input class="input-modal form-control" type="email" id="email" name="email" placeholder="E-mail" required>
-						<?php //echo '<center><div class="aviso"><strong>Você será direcionado para um de nossos atendentes para finalizar a compra.</strong></div</center>'; ?>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Fechar</button>
-						<button type="submit" class="btn btn-outline-success" target="_blank">
-							<span >
-								<span >
-									<i class="fab fa-whatsapp"></i> 
-								</span>
-								<span > Confirmar </span>
-							</span>
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div-->
-	<!-- Fim do Modal -->
+	
   <script src="./js/funcao.js"></script>
 </html>
