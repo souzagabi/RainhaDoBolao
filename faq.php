245feaaa503107355d4005b405eaf3cc876c9753
souzagabi <souza_gabi@yahoo.com.br>
2023-02-03 04:42:39 -0300

    ccommit faq

---
+++ faq.php
@@ -0,0 +1,29 @@
+<html lang="pt-BR">
+<head>
+    <?php
+        require_once('partsIndex/header.html');
+    ?>
+    <style>
+        .container{
+            margin-top: 100px;
+            margin-left: 150px;
+            text-align: justify;
+        }
+    </style>
+</head>
+<body>
+    <?php require_once('partsIndex/top.html'); ?>
+
+    <div class="container faq">
+        <p>    
+            O trecho padrão original de Lorem Ipsum, usado desde o século XVI, está reproduzido abaixo para os interessados. Seções 1.10.32 e 1.10.33 de "de Finibus Bonorum et Malorum" de Cicero também foram reproduzidas abaixo em sua forma exata original, acompanhada das versões para o inglês da tradução feita por H. Rackham em 1914.
+        </p>
+        <p>   
+            Onde posso conseguí-lo?
+            Existem muitas variações disponíveis de passagens de Lorem Ipsum, mas a maioria sofreu algum tipo de alteração, seja por inserção de passagens com humor, ou palavras aleatórias que não parecem nem um pouco convincentes. Se você pretende usar uma passagem de Lorem Ipsum, precisa ter certeza de que não há algo embaraçoso escrito escondido no meio do texto. Todos os geradores de Lorem Ipsum na internet tendem a repetir pedaços predefinidos conforme necessário, fazendo deste o primeiro gerador de Lorem Ipsum autêntico da internet. Ele usa um dicionário com mais de 200 palavras em Latim combinado com um punhado de modelos de estrutura de frases para gerar um Lorem Ipsum com aparência razoável, livre de repetições, inserções de humor, palavras não características, etc.
+        </p>
+    </div>
+    <?php require_once('partsIndex/footer.html'); ?>
+</body>
+
+</html>
